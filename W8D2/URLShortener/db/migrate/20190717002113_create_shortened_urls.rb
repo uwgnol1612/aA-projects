8aff25b8b5702de0cd5ea987c2b5f4c2809b1b9b
LongMcFarlin <longwu2011@gmail.com>
2019-07-16 20:34:42 -0600

    URL Shortener

---
+++ W8D2/URLShortener/db/migrate/20190717002113_create_shortened_urls.rb
@@ -0,0 +1,10 @@
+class CreateShortenedUrls < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shortened_urls do |t|
+      t.string :long_url, null:false 
+      t.string :short_url, null:false 
+      t.integer :submitter_id
+    end
+    add_index :shortened_urls, :short_url, unique:true 
+  end
+end
