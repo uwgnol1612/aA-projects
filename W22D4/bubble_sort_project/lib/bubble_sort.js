f3331d0f792e19b6cc187b2457f14632b78e733c
LongMcFarlin <longwu2011@gmail.com>
2019-11-28 20:00:02 -0700

    sort project part 1

---
+++ W22D4/bubble_sort_project/lib/bubble_sort.js
@@ -0,0 +1,31 @@
+function swap(array, idx1, idx2) {
+    let temp = array[idx2]
+        array[idx2] = array[idx1]
+        array[idx1] = temp
+
+    return array
+
+}
+
+function bubbleSort(array) {
+    let sorted = false;
+
+    while (!sorted) {
+        sorted = true;
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                sorted = false 
+            }
+        }
+    } 
+    return array
+
+}
+
+
+module.exports = {
+    bubbleSort,
+    swap
+};
